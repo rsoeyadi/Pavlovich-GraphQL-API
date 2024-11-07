c4217d5766f15633553a94d036f7759b78df9237
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-11-07 18:40:45 -0500

    connection pooling for db + basic graphql schema, routed w/ express

---
+++ server.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import { createHandler } from 'graphql-http/lib/use/express';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import 'dotenv/config'
+import pg from 'pg'
+
+// create a pool to reuse connections instead of creating direct connections for every request
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URI,
+})
+
+const createPool = async (): Promise<void> => {
+  const client = await pool.connect()
+  console.log({ client })
+}
+
+createPool()
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType', // just used to uniquely identify for debugging purposes (only for server)
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve(): string {
+          return 'world';
+        },
+      },
+    }
+  })
+})
+
+var app = express() // using express to create HTTP server
+app.use('/graphql', createHandler({ schema })); // this will route requests to /graphql to our createHandler() function
+app.listen(4000, (): void => {
+  console.log("Running on port 4000")
+})
